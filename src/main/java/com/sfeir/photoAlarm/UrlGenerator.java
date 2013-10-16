99fb88e81303b20be9e769518a53727e13076099
Walid Djebali <waliddjebali@administrateursfeirs-MacBook-Pro-3.local>
2013-10-16 14:33:41 +0200

    First commit

---
+++ src/main/java/com/sfeir/photoAlarm/UrlGenerator.java
@@ -0,0 +1,45 @@
+package com.sfeir.photoAlarm;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.appengine.api.blobstore.BlobstoreService;
+import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
+import com.google.appengine.api.blobstore.UploadOptions;
+
+/**
+ * Servlet implementation class UrlGenerator
+ */
+public class UrlGenerator extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UrlGenerator() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		 BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
+		 UploadOptions uploadOptions = UploadOptions.Builder.withGoogleStorageBucketName("bucket-photoalarm-sfeir");
+		 String uploadUrl = blobstoreService.createUploadUrl("/upload", uploadOptions);
+		    
+		 response.getWriter().write(uploadUrl);
+	}
+
+}
