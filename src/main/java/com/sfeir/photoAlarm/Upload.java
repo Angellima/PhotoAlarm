51a628194b09f8deb2481c39a13b82f581dcb8e6
waliddjebali <waliddjebali@192.168.1.105>
2013-10-18 10:59:35 +0200

    save blovkey

--- src/main/java/com/sfeir/photoAlarm/Upload.java
+++ src/main/java/com/sfeir/photoAlarm/Upload.java
@@ -1,6 +1,9 @@
 package com.sfeir.photoAlarm;
 
 import java.io.IOException;
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Map;
 
 import javax.servlet.ServletException;
@@ -12,6 +15,9 @@ import com.google.appengine.api.blobstore.BlobKey;
 import com.google.appengine.api.blobstore.BlobstoreService;
 import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
 import com.google.appengine.api.blobstore.UploadOptions;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
 
 public class Upload extends HttpServlet {
     private BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
@@ -28,9 +34,23 @@ public class Upload extends HttpServlet {
             res.sendRedirect("/serve?blob-key=" + blobKey.getKeyString());
         }*/
         
+       
+        saveBlobKey(blobKey.getKeyString());
+        
+        
         UploadOptions uploadOptions = UploadOptions.Builder.withGoogleStorageBucketName("bucket-photoalarm-sfeir");
         String uploadUrl = blobstoreService.createUploadUrl("/on_upload_success", uploadOptions);
         
         
+    }
+    
+    public void saveBlobKey(String blobKey){
+    	DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+
+    	Entity employee = new Entity("Photo");
+    	employee.setProperty("blobKey", blobKey);
+    	employee.setProperty("datePhoto",Calendar.getInstance().getTimeInMillis() );
+
+    	datastore.put(employee);
     }
 }
